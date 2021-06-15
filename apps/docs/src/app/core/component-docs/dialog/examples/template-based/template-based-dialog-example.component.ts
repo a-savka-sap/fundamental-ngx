527023c6c3c70742acd779069f40556417c206db
I505460 <lokanathan.k@sap.com>
2021-06-15 17:54:31 +0530

    fix(core): add ariaLabelledby to the dialog header for screen reader
    fix(core): add ariaLabelledby to the dialog header for screen reader

--- apps/docs/src/app/core/component-docs/dialog/examples/template-based/template-based-dialog-example.component.ts
+++ apps/docs/src/app/core/component-docs/dialog/examples/template-based/template-based-dialog-example.component.ts
@@ -13,6 +13,7 @@ export class TemplateBasedDialogExampleComponent {
     openDialog(dialog: TemplateRef<any>): void {
         const dialogRef = this._dialogService.open(dialog, {
             responsivePadding: true,
+            ariaLabelledBy: 'fd-dialog-header-10',
             ariaDescribedBy: 'fd-dialog-body-10'
         });
 
